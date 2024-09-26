d1d443e2fe63b1f4d8c8230da5747a49206a813b
Bolero-fk <fuku_0000@outlook.jp>
2024-09-26 16:21:25 +0900

    test add file

---
+++ test/Math/enumerate_divisor/test.cpp
@@ -0,0 +1,26 @@
+// competitive-verifier: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_D
+
+#include "Math/test.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    vector<long long> divisor = enumerate_divisor(c);
+
+    long long ans = 0;
+
+    for (long long n : divisor)
+    {
+        if (a <= n && n <= b)
+            ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
