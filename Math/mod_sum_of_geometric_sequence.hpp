5fff07ef6c39519b487df5b89f5e47285af2fe83
Bolero-fk <fuku_0000@outlook.jp>
2024-09-30 16:15:05 +0900

    add document of sum of geometric sequence

--- Math/mod_sum_of_geometric_sequence.hpp
+++ Math/mod_sum_of_geometric_sequence.hpp
@@ -1,14 +1,20 @@
-/// @brief 等比数列の総和をmodつきで計算する
 template <class mint>
 mint sum_of_geometric_sequence(mint a, mint r, long long n)
 {
+    assert(0 < n);
+
     if (n == 1)
+    {
         return a;
+    }
 
     mint x = sum_of_geometric_sequence(a, r, n / 2);
 
     mint ret = x + r.pow(n / 2) * x;
     if (n & 1)
+    {
         ret = (a + r * ret);
+    }
+
     return ret;
 };
---
+++ docs/Math/mod_sum_of_geometric_sequence.md
@@ -0,0 +1,19 @@
+---
+title: 等比数列の総和 (mod)
+documentation_of: //Math/mod_sum_of_geometric_sequence.hpp
+---
+
+# sum_of_geometric_sequence
+
+```cpp
+mint sum_of_geometric_sequence(mint a, mint r, long long n)
+```
+
+初項$a$、公比$r$、項数$n$からなる等比数列の部分和 $\sum_{i=0}^{n-1} a \cdot r^i$ を計算し、その値を返します。
+
+**制約**
+- $0 < n$
+
+**計算量**
+
+- $O(\log n)$
