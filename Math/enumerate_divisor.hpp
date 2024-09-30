641e1b97a51a90a6365a2289977fcd3020ec4954
Bolero-fk <fuku_0000@outlook.jp>
2024-09-30 14:40:12 +0900

    add document of enumerate divisor

--- Math/enumerate_divisor.hpp
+++ Math/enumerate_divisor.hpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-/// @brief 約数列挙
-/// @param n 約数を列挙したい数
-/// @return nの約数(昇順)
 vector<long long> enumerate_divisor(long long n)
 {
+    assert(0 < n);
     vector<long long> dst;
     for (long long i = 1; i * i <= n; i++)
     {
---
+++ docs/Math/enumerate_divisor.md
@@ -0,0 +1,20 @@
+---
+title: 約数列挙
+documentation_of: //Math/enumerate_divisor.hpp
+---
+
+# enumerate_divisor
+
+```cpp
+vector<long long> enumerate_divisor(long long n)
+```
+
+入力された値の約数を昇順にしたvectorを返します。
+
+**制約**
+
+- $0 \lt n$
+
+**計算量**
+
+- $O(\sqrt n)$
