08ae9b8837e4cfa0e3e46d967582097d797c45ed
Bolero-fk <fuku_0000@outlook.jp>
2024-09-30 19:12:18 +0900

    add document of enumerate primes

--- Math/enumerate_primes.hpp
+++ Math/enumerate_primes.hpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/// @brief エラトステネスの篩による素数列挙
-/// 計算量O(√n)
 vector<long long> enumerate_primes(long long n)
 {
+    assert(0 < n);
+
     vector<long long> primes;
     vector<bool> isChecked(n + 1, false);
     isChecked[0] = isChecked[1] = true;
---
+++ docs/Math/enumerate_primes.md
@@ -0,0 +1,20 @@
+---
+title: 素数列挙
+documentation_of: //Math/enumerate_primes.hpp
+---
+
+# enumerate_primes
+
+```cpp
+vector<long long> enumerate_primes(long long n)
+```
+
+エラトステネスの篩によって`n`以下の素数を計算し、返します。
+
+**制約**
+
+- $0 \lt n$
+
+**計算量**
+
+- $O(n \log \log n)$
