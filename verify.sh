22396f2458d4b3e0c496c487f63c581d4aff4d27
Bolero-fk <fuku_0000@outlook.jp>
2025-12-08 21:51:11 +0900

    yukicoder tokenをインポートする設定を自動で実行するように変更した

--- verify.sh
+++ verify.sh
@@ -1,1 +1,10 @@
-competitive-verifier oj-resolve --config ./.verify-helper/config.toml --include "$@" | competitive-verifier verify --verify-json /dev/stdin
+#!/usr/bin/env bash
+set -eu
+
+# このリポジトリ専用の秘密設定があれば読み込む
+if [ -f "./.env.local" ]; then
+  . "./.env.local"
+fi
+
+competitive-verifier oj-resolve --config ./.verify-helper/config.toml --include "$@" \
+  | competitive-verifier verify --verify-json /dev/stdin
